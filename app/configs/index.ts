8fea4f89cb31dc98539dc8db5776c3c169cffe6b
Misdar <50019528+misdarmanto@users.noreply.github.com>
2025-02-11 15:06:13 +0700

    Update index.ts

--- app/configs/index.ts
+++ app/configs/index.ts
@@ -4,6 +4,6 @@ export const appConfig = {
   localStorage: {
     appKey: "appKey",
   },
-  baseUrl: "https://jasaapk.us/freshx/api/v1", //"http://192.168.1.20:8000/api/v1", //'https://jasaapk.us/fresh/api/v1'
+  baseUrl: "http://192.168.1.20:8000/api/v1", 
   colors: COLORS,
 };
